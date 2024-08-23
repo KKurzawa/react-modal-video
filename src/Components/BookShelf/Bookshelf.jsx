291b0fd3b32b591c13beb7b5de2826e4f1e9e263
Kris Kurzawa <kurzawa1@yahoo.com>
2024-08-23 15:00:06 -0400

    added colors and settimeout

--- src/Components/BookShelf/Bookshelf.jsx
+++ src/Components/BookShelf/Bookshelf.jsx
@@ -1,9 +1,10 @@
 import './Bookshelf.css'
 import { Component } from "react";
 import ModalVideo from "react-modal-video";
+import Videos from '../../Utils/VideoData';
 import "react-modal-video/scss/modal-video.scss";
 
-export class videoModal extends Component {
+export class bookshelf extends Component {
     constructor() {
         super();
         this.state = {
@@ -13,63 +14,31 @@ export class videoModal extends Component {
     }
 
     openModal() {
-        this.setState({ isOpen: true });
+        setTimeout(() => {
+            this.setState({ isOpen: true });
+        }, 500)
+        // this.setState({ isOpen: true });
     }
     render() {
         return (
             <main className='flex flex-col items-center w-full mt-40'>
                 <article className='flex justify-center items-baseline h-fit'>
-                    <div>
-                        <ModalVideo
-                            channel="vimeo"
-                            isOpen={this.state.isOpen}
-                            videoId="102435491"
-                            onClose={() => this.setState({ isOpen: false })}
-                        />
-                        <button onClick={this.openModal} id='Something' className='flex justify-center items-center book w-10 h-[20rem] border-2 rounded-md border-black'>Something or Incomplete</button>
-                    </div>
-                    <div>
-                        <ModalVideo
-                            channel="vimeo"
-                            isOpen={this.state.isOpen}
-                            videoId="102435491"
-                            onClose={() => this.setState({ isOpen: false })}
-                        />
-                        <button onClick={this.openModal} id='Missing' className='flex justify-center items-center book w-10 h-[20rem] border-2 rounded-md border-black'>Missing or Incomplete</button>
-                    </div>
-                    <div>
-                        <ModalVideo
-                            channel="vimeo"
-                            isOpen={this.state.isOpen}
-                            videoId="102435491"
-                            onClose={() => this.setState({ isOpen: false })}
-                        />
-                        <button onClick={this.openModal} id='Cover' className='flex justify-center items-center book w-10 h-[21rem] border-2 rounded-md border-black'>Cover Your Eyes</button>
-                    </div>
-                    <div>
-                        <ModalVideo
-                            channel="vimeo"
-                            isOpen={this.state.isOpen}
-                            videoId="102435491"
-                            onClose={() => this.setState({ isOpen: false })}
-                        />
-                        <button onClick={this.openModal} id='Yaga' className='flex justify-center items-center book w-10 h-[20rem] border-2 rounded-md border-black'>Yaga Intro</button>
-                    </div>
-                    <div>
-                        <ModalVideo
-                            channel="vimeo"
-                            isOpen={this.state.isOpen}
-                            videoId="102435491"
-                            onClose={() => this.setState({ isOpen: false })}
-                        />
-                        <button onClick={this.openModal} id='Uncle' className='flex justify-center items-center book w-10 h-[20.5rem] border-2 rounded-md border-black'>Uncle Andre Excerpt</button>
-                    </div>
+                    {Videos.map((item) => (
+                        <section key={item.key}>
+                            <ModalVideo
+                                channel={item.channel}
+                                isOpen={this.state.isOpen}
+                                videoId={item.videoId}
+                                onClose={() => this.setState({ isOpen: false })}
+                            />
+                            <button onClick={this.openModal} id={item.id} className='flex justify-center items-center book w-10 h-[20rem] border-2 rounded-md border-black'>{item.title}</button>
+                        </section>
+                    ))}
                 </article>
                 <article className='border-b-2 w-96 h-fit border-black'></article>
             </main>
-
         );
     }
 }
 
-export default videoModal;
+export default bookshelf;
